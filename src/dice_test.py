c52031c036327f0886ac9856cc85118d1bafabf0
Quan Do <biquando@gmail.com>
2025-06-16 12:09:44 -0700

    Add remaining Dice.ml tests
    Omitted are:
    - test_caesar_iterate (haven't implemented `iterate`)
    - test_alarm_2 (too large)
    - test_pmc1 (too large)
    - test_pmc2 (too large)
    - test_list_distribution (too large)
    - test_bdd (contains unsupported bdd features)

--- src/dice_test.py
+++ src/dice_test.py
@@ -223,6 +223,30 @@ let _y0 = if ( nth_bit(int(2,1), b >> 1)) then _y1 else true in
 _x0 <=> _y0"""
     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(1.0, rel=0.1)
 
+def test_unif_1(test_parser: lark.Lark) -> None:
+    text1 = "let u = uniform(4, 0, 10) in u < int(4, 4)"
+    text2 = "let d = discrete(0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1) in d < int(4, 4)"
+    prob1 = parse_string(text1, test_parser)[BoolType(True)]
+    prob2 = parse_string(text2, test_parser)[BoolType(True)]
+    assert prob1 == pytest.approx(prob2, rel=0.1)
+    assert prob1 == pytest.approx(0.4, rel=0.1)
+
+def test_unif_2(test_parser: lark.Lark) -> None:
+    text1 = "let u = uniform(3, 2, 6) in u == int(3, 0)"
+    text2 = "let d = discrete(0., 0., 0.25, 0.25, 0.25, 0.25) in d == int(3, 0)"
+    prob1 = parse_string(text1, test_parser)[BoolType(False)]
+    prob2 = parse_string(text2, test_parser)[BoolType(False)]
+    assert prob1 == pytest.approx(prob2, rel=0.1)
+    assert prob1 == pytest.approx(1.0, rel=0.1)
+
+def test_unif_3(test_parser: lark.Lark) -> None:
+    text1 = "let u = uniform(3, 3, 4) in u == int(3, 3)"
+    text2 = "let d = discrete(0., 0., 0., 1., 0.) in d == int(3, 3)"
+    prob1 = parse_string(text1, test_parser)[BoolType(True)]
+    prob2 = parse_string(text2, test_parser)[BoolType(True)]
+    assert prob1 == pytest.approx(prob2, rel=0.1)
+    assert prob1 == pytest.approx(1.0, rel=0.1)
+
 def test_unif_4(test_parser: lark.Lark) -> None:
     text = """
     let u = uniform(2, 1, 4) in
@@ -237,6 +261,10 @@ def test_binom_1(test_parser: lark.Lark) -> None:
 def test_binom_2(test_parser: lark.Lark) -> None:
     text = """let b = binomial(5, 29, 0.5) in b <= int(5, 14)"""
     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.5, rel=0.1)
+
+def test_binom_3(test_parser: lark.Lark) -> None:
+    text = """let b = binomial(3, 0, 0.5) in b == int(3, 0)"""
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(1.0, rel=0.1)
 
 def test_binom_4(test_parser: lark.Lark) -> None:
     text = """let b = binomial(3, 1, 0.3) in b == int(3, 1)"""
@@ -344,7 +372,7 @@ def test_caesar(test_parser: lark.Lark) -> None:
     let tmp = sendchar(key, int(2, 2)) in
     let tmp = sendchar(key, int(2, 3)) in
     key == int(2, 0)"""
-    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.25, rel=0.1)
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.25, rel=0.3)
 
 ##### This test case is removed because it uses an "iterate" keyword, but idk
 ##### what that does :(
@@ -377,6 +405,129 @@ def test_burglary(test_parser: lark.Lark) -> None:
     let temp = observe mary in
     burglary"""
     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.284172, rel=0.3)
+
+def test_alarm(test_parser: lark.Lark) -> None:
+    text = """
+    let earthquake  = flip 0.0001 in
+    let burglary  = flip 0.001 in
+    let alarm  = earthquake || burglary in
+    let phoneWorking  = if earthquake then flip 0.7 else flip 0.99 in
+    let maryWakes  = if alarm then
+        (if earthquake then flip 0.8 else flip 0.6)
+        else flip 0.2 in
+    let called  = (maryWakes && phoneWorking) in
+    let tmp = observe called in
+    burglary
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(2969983.0 / 992160802.0, rel=0.3)
+
+def test_murder(test_parser: lark.Lark) -> None:
+    text = """
+    fun mystery() {
+        let aliceDunnit  = flip 0.3 in
+        let withGun = if aliceDunnit then flip 0.03 else flip 0.8 in
+        (aliceDunnit, withGun)
+    }
+
+    fun gunFoundAtScene(gunFound: bool) {
+        let res = mystery() in
+        let aliceDunnit = fst res in
+        let withGun = snd res in
+        let obs = if withGun then gunFound else !gunFound in
+        let tmp = observe obs in
+        aliceDunnit
+    }
+
+    gunFoundAtScene(true)
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(9.0 / 569.0, rel=0.3)
+
+def test_evidence1(test_parser: lark.Lark) -> None:
+    text = """
+    let evidence = flip 0.5 in
+    let coin  = flip 0.5 in
+    if evidence then
+        let tmp = observe coin in evidence
+        else evidence
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(1.0 / 3.0, rel=0.1)
+
+def test_evidence2(test_parser: lark.Lark) -> None:
+    text = """
+    let evidence = flip 0.5 in
+        if evidence then
+            let coin1  = flip 0.5 in
+            let tmp = observe coin1 in
+            coin1
+        else
+            flip 0.5
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(2.0 / 3.0, rel=0.1)
+
+def test_grass(test_parser: lark.Lark) -> None:
+    text = """
+    let cloudy  = flip 0.5 in
+    let rain  = if cloudy then flip 0.8 else flip 0.2 in
+    let sprinkler  = if cloudy then flip 0.1 else flip 0.5 in
+    let temp1  = flip 0.7 in
+    let wetRoof  = (temp1 && rain) in
+    let temp2  = flip 0.9 in
+    let temp3  = flip 0.9 in
+    let wetGrass  = ((temp2 && rain) || ( temp3 && sprinkler)) in
+    let tmp = observe wetGrass in
+    rain
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(509.0 / 719.0, rel=0.1)
+
+def test_cancer(test_parser: lark.Lark) -> None:
+    text = """
+    let Pollution = discrete(0.500000,0.400000,0.100000) in
+    let Smoker = flip 0.300000 in
+    let Cancer =
+     if ((Pollution == int(2, 2))) then
+      (if (Smoker) then (flip 0.030000) else (flip 0.0010000))
+     else (if ((Pollution == int(2, 1))) then
+       (if (Smoker) then (flip 0.030000) else (flip 0.0010000))
+     else (if (Smoker) then (flip 0.050000) else (flip 0.020000))) in
+    let Dyspnoea = if (Cancer) then (flip 0.650000) else (flip 0.300000) in
+    let Xray = if (Cancer) then (flip 0.900000) else (flip 0.200000) in
+    Xray
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(42709.0 / 200000.0, rel=0.1)
+
+def test_caesar_2(test_parser: lark.Lark) -> None:
+    text = """
+    fun drawChar(key: int(5), observation: int(5)) {
+        let drawnChar = discrete(0.08167, 0.01492, 0.02782, 0.04253, 0.04378, 0.02228, 0.02015, 0.06094, 0.06966, 0.0153, 0.0772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074) in
+        let encrypted = key + drawnChar  in
+        observe observation == encrypted
+    }
+    let key1  = discrete(0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538) in
+    let a = drawChar(key1, int(5, 6)) in
+    key1 == int(5, 5)
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(1113032.0 / 315312455.0, abs=0.1)
+
+### This test is removed because it's just too big
+# def test_alarm_2(test_parser: lark.Lark) -> None:
+#     text = """
+#     ...
+#     """
+#     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.281037656, rel=0.1)
+
+### This test is removed because it's just too big
+# def test_pmc1(test_parser: lark.Lark) -> None:
+#     text = """
+#     ...
+#     """
+#     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(1023.0 / 2048.0, rel=0.1)
+
+### This test is removed because it's just too big
+# def test_pmc2(test_parser: lark.Lark) -> None:
+#     text = """
+#     ...
+#     """
+#     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(31.0 / 64.0, rel=0.1)
 
 def test_double_flip(test_parser: lark.Lark) -> None:
     text = """
@@ -411,6 +562,20 @@ let tmp = observe candy in
 coin1 == int(6, 10)
 """
     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.45, rel=0.1)
+
+def test_recursion(test_parser: lark.Lark) -> None:
+    text = """
+    fun f() {
+        flip 0.5
+    }
+
+    fun flip_n(n: int(4)): bool {
+        if n == int(4, 0) then true else f() && flip_n(n - int(4, 1))
+    }
+
+    flip_n(int(4, 3))
+    """
+    assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.5 ** 3., rel=0.1)
 
 def test_caesar_recursive(test_parser: lark.Lark) -> None:
     text = """
@@ -483,6 +648,13 @@ def test_list_recursion(test_parser: lark.Lark) -> None:
   """
     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(1.0, rel=0.1)
 
+### This test is removed because it's just too big
+# def test_list_distribution(test_parser: lark.Lark) -> None:
+#     text = """
+#     ...
+#     """
+#     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(..., rel=0.1)
+
 def test_list_ex(test_parser: lark.Lark) -> None:
     text = """
     let xs = [flip 0.2, flip 0.4] in
@@ -490,3 +662,21 @@ def test_list_ex(test_parser: lark.Lark) -> None:
     head ys
   """
     assert parse_string(text, test_parser)[BoolType(True)] == pytest.approx(0.30000000000000004, rel=0.1)
+
+def test_lte_name(test_parser: lark.Lark) -> None:
+    text1 = "uniform(3, 0, 8) <= uniform(3, 0, 8)"
+    text2 = "let a = uniform(3, 0, 8) in let b = uniform(3, 0, 8) in a <= b"
+    prob1 = parse_string(text1, test_parser)[BoolType(True)]
+    prob2 = parse_string(text2, test_parser)[BoolType(True)]
+    assert prob1 == pytest.approx(prob2, rel=0.2)
+
+def test_lt_name(test_parser: lark.Lark) -> None:
+    text1 = "uniform(3, 0, 8) < uniform(3, 0, 8)"
+    text2 = "let a = uniform(3, 0, 8) in let b = uniform(3, 0, 8) in a < b"
+    prob1 = parse_string(text1, test_parser)[BoolType(True)]
+    prob2 = parse_string(text2, test_parser)[BoolType(True)]
+    assert prob1 == pytest.approx(prob2, rel=0.2)
+
+### This test is removed because we don't support bdd_exists
+# def test_bdd(test_parser: lark.Lark) -> None:
+#     ...
